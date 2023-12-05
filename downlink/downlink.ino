c4a9b81b2fdf4ea6b0d73f33ba4966a85bdb9092
zczqxc5 <146037962+zczqxc5@users.noreply.github.com>
2023-12-05 17:49:02 +0000

    Update downlink.ino

--- downlink/downlink.ino
+++ downlink/downlink.ino
@@ -1,5 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
 
 // 替换为您的WiFi网络名称和密码
 const char* ssid = "CE-Hub-Student";
@@ -77,11 +78,25 @@ void reconnect() {
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (unsigned int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
+  // 解析JSON数据
+  StaticJsonDocument<10000> doc; // 适当调整缓冲区大小以适应您的消息大小
+  deserializeJson(doc, payload, length);
+
+  // 从JSON文档中提取数据
+  float humidity = doc["uplink_message"]["decoded_payload"]["humidity"];
+  float light = doc["uplink_message"]["decoded_payload"]["light"];
+  int rain = doc["uplink_message"]["decoded_payload"]["rain"];
+  float temperature = doc["uplink_message"]["decoded_payload"]["temperature"];
+
+  // 打印提取的数据
+  Serial.print("Humidity: ");
+  Serial.println(humidity);
+  Serial.print("Light: ");
+  Serial.println(light);
+  Serial.print("Rain: ");
+  Serial.println(rain);
+  Serial.print("Temperature: ");
+  Serial.println(temperature);
+
+  // 其他处理您需要的代码
 }
